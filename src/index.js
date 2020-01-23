edd27e30d7c286b254453fb4ace5fe53b2d9b174
Patricio R <patricio.raschetti@gmail.com>
2020-01-22 21:47:20 -0300

    Setup webpack Dev Server (environment I)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import squaree, { add, name } from './utilities';
 import scream from './scream';
 console.log('index.js');
 
-console.log(add(20, 20));
+console.log(add(22, 20));
 console.log(scream(name));
 console.log(name);
 console.log(squaree(10));
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
         }]
     },
     devServer: {
-        watchContentBase: true
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/',
+        //watchContentBase: true
     }
 }
